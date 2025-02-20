6ff181b8efc3bfce1d65650f5abea6dfbc811099
arisa2545 <arisa8533@gmail.com>
2025-02-20 14:50:10 +0700

    config module federation on host app for use Button component

--- host-app/vite.config.ts
+++ host-app/vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "host-app",
+      remotes: {
+        remote_app: "http://localhost:4173/assets/remoteEntry.js",
+      },
+      shared: ["react"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })

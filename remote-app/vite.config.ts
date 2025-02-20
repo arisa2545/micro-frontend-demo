d765a9728e6b142e50ffb00fcc735bd2bb7ff003
arisa2545 <arisa8533@gmail.com>
2025-02-20 14:10:40 +0700

    config module federation to expose component

--- remote-app/vite.config.ts
+++ remote-app/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 5173 },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/components/Button.tsx",
+      },
+      shared: ["react"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
